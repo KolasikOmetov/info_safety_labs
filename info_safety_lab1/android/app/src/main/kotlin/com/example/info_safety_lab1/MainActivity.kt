2cada3bb2574c8b4ae0b0690849959bab31fbc88
KolasikOmetov <42410594+KolasikOmetov@users.noreply.github.com>
2022-09-20 23:32:06 +0400

    feat: init info_safety_lab1

---
+++ info_safety_lab1/android/app/src/main/kotlin/com/example/info_safety_lab1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.info_safety_lab1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ info_safety_lab1/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:info_safety_lab1/pages/home_page.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lab 1',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
