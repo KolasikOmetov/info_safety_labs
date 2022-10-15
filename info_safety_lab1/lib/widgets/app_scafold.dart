4c0c3e3af0a215d5b1f93751e8c75a52a5b8a6f1
KolasikOmetov <42410594+KolasikOmetov@users.noreply.github.com>
2022-10-15 19:25:24 +0400

    feat: add about program

--- info_safety_lab1/lib/widgets/app_scafold.dart
+++ info_safety_lab1/lib/widgets/app_scafold.dart
@@ -13,6 +13,89 @@ class AppScaffold extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
         actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: (_) => showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Developer:',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Омётов Николай ПИбд-41',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      Text(
+                        'Вариант:',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '18',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      Text(
+                        'Используемый режим шифрования алгоритма DES:',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'CBC',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      Text(
+                        'Добавление к ключу случайного значения:',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Нет',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      Text(
+                        'Используемый алгоритм хеширования:',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'SHA',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      Text(
+                        'Ограничения на выбираемые пароли:',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Наличие цифр, знаков препинания и знаков арифметических операций',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            itemBuilder: (BuildContext context) {
+              return <PopupMenuEntry<String>>[
+                PopupMenuItem(
+                  value: 'About Program',
+                  child: Text(
+                    'About Program',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                )
+              ];
+            },
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Help',
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
           TextButton(
             onPressed: () => exitProgram(context),
             child: Text(
