7916f6acf31fa65690ed309b088e5d4eb632774d
KolasikOmetov <42410594+KolasikOmetov@users.noreply.github.com>
2022-12-09 20:44:55 +0400

    feat: hooray it's correct

--- info_safety_lab2/test.dart
+++ info_safety_lab2/test.dart
@@ -12,43 +12,91 @@ const sBoxes = [
   [1, 15, 13, 0, 5, 7, 10, 4, 9, 2, 3, 14, 6, 11, 8, 12],
 ];
 
-final Uint32List key = Uint32List.fromList([32000, 400, 500, 600, 700, 800, 900, 1100]);
+final Uint32List key = Uint32List.fromList([
+  0xF904C1E2,
+  0xDE7C1DE4,
+  0x57E8E57F,
+  0xB4650206,
+  0x85CC1C28,
+  0x9A922C2E,
+  0x03454647,
+  0x10E50CE0,
+]);
 
 void main() {
-  Uint32List byteContent = Uint32List.fromList('ку'.codeUnits);
-  List<int> bits = [];
-  for (var element in byteContent) {
-    bits.addAll(element.toBits(32));
+  Uint16List unicodes = Uint16List.fromList('hello, world!'.codeUnits);
+  print(unicodes);
+
+  final List<int> biteContent = [];
+  for (int code in unicodes) {
+    final bites = code.toBits(16);
+    biteContent.addAll(bites.sublist(8));
+    biteContent.addAll(bites.sublist(0, 8));
   }
 
-  print(bits.length);
-  print(bits.length % 64);
+  final blocks =
+      List<List<int>>.generate(biteContent.length ~/ 64, (index) => biteContent.sublist(index * 64, (index + 1) * 64));
 
-  if (bits.length % 64 != 0) {
-    bits.addAll(0.toBits(32));
+  int remain = biteContent.length % 64;
+  if (remain != 0) {
+    blocks.add([...biteContent.sublist(biteContent.length ~/ 64 * 64), ...List<int>.generate(64 - remain, (_) => 0)]);
   }
 
-  List<int> data = bits.sublist(0, 2);
+  final cryptoData = <int>[];
+  final decryptedData = <int>[];
 
-  print('dataBefore');
-  print(data);
-
-  for (int round = 0; round < 32; round++) {
-    data = calcRoundGOST(data, key[getKeyIndex(round)]);
+  for (final block in blocks) {
+    final bytesBlock = block.bitsToBytes();
+    print(bytesBlock);
+    final Uint8List cryptoResult = process(bytesBlock);
+    cryptoData.addAll(cryptoResult.toBits(8));
   }
 
-  print('dataAfter');
-  print(data);
+  final List<List<int>> blocksEncrypted =
+      List<List<int>>.generate(cryptoData.length ~/ 64, (index) => cryptoData.sublist(index * 64, (index + 1) * 64));
+
+  for (final List<int> block in blocksEncrypted) {
+    final bytesBlock = block.bitsToBytes();
+    print(bytesBlock);
+    final Uint8List decryptoResult = process(bytesBlock, isEncrypte: false);
+    print('block: ${blocksEncrypted.join()}');
+    print('dlock: ${decryptoResult.toBits(8).join()}');
+    decryptedData.addAll(decryptoResult.toBits(8));
+  }
+
+  print('Encrypted');
+
+  final charsBlocksEncrypted = cryptoData.bitsToChars(unicodes.length);
+  print(charsBlocksEncrypted);
+  try {
+    print(String.fromCharCodes(charsBlocksEncrypted));
+  } catch (e) {
+    print(e);
+  }
+
+  final rawCharsBlocksDecrypted = decryptedData.bitsToChars(unicodes.length);
+  biteContent.clear();
+  for (int code in rawCharsBlocksDecrypted) {
+    final bites = code.toBits(16);
+    biteContent.addAll(bites.sublist(8));
+    biteContent.addAll(bites.sublist(0, 8));
+  }
+
+  final charsBlocksDecrypted = biteContent.bitsToChars(unicodes.length);
+
+  try {
+    print(String.fromCharCodes(charsBlocksDecrypted));
+  } catch (e) {
+    print(e);
+  }
 }
 
 int getKeyIndex(int round, [bool isEncript = false]) {
-  return isEncript
-      ? (round < 24)
-          ? round % 8
-          : 7 - (round % 8)
-      : (round < 8)
-          ? round % 8
-          : 7 - (round % 8);
+  if (isEncript) {
+    return round < 24 ? (round % 8) : 7 - (round % 8);
+  }
+
+  return round < 8 ? (round % 8) : 7 - (round % 8);
 }
 
 extension IntToBits on int {
@@ -73,54 +121,82 @@ extension BitsToInt on List<int> {
     }
     return result;
   }
+
+  Uint8List bitsToBytes() =>
+      Uint8List.fromList(List<int>.generate(8, (index) => sublist(index * 8, (index + 1) * 8).toInt()));
+
+  List<int> bitsToChars(int length) =>
+      List<int>.generate(length, (index) => sublist(index * 16, (index + 1) * 16).toInt());
 }
 
-List<int> calcRoundGOST(List<int> block, int key) {
-  int leftPartInit = block[0];
-  print('left');
-  print(leftPartInit.to2);
-  int rightPartInit = block[1];
-  print('right');
-  print(rightPartInit.to2);
-
-  int rightPartResult = (rightPartInit + key) % 32; // 25 - 32 round  key starts from 7
-  print('key sum');
-  print(rightPartResult.to2);
-  List<int> sParts = rightPartResult.toBits(32);
-  print(sParts.join());
-
-  List<int> sBoxesResults = List<int>.generate(sBoxes.length, (int index) {
-    final int sBoxIndex =
-        sParts[index * 4] * 8 + sParts[index * 4 + 1] * 4 + sParts[index * 4 + 2] * 2 + sParts[index * 4 + 3];
-    final int sBoxResult = sBoxes[index][sBoxIndex];
-    return sBoxResult;
-  });
-  print('sBoxesResults');
-  print(sBoxesResults);
-
-  List<int> sBoxResult = [];
-
-  for (int i = 0; i < sBoxesResults.length; i++) {
-    int element = sBoxesResults[i];
-    sBoxResult.addAll(element.toBits(4));
+extension BytesToInt on Uint8List {
+  int fromListToInt() {
+    List<int> result = [];
+    for (int i = 0; i < length; i++) {
+      result.addAll(elementAt(i).toBits(8));
+    }
+    return result.toInt();
   }
-  print('sBoxesResult');
-  print(sBoxResult.join());
 
-  final intSBoxResult = sBoxResult.toInt();
-  print(intSBoxResult);
+  List<int> toBits(int radix) {
+    final List<int> allBits = <int>[];
+    for (int i = 0; i < length; i++) {
+      final List<int> bits = <int>[];
+      int number = elementAt(i);
+      for (int j = 0; j < radix; j++) {
+        bits.add(number & 1);
+        number = number >> 1;
+      }
+      allBits.addAll(bits.reversed);
+    }
+    return allBits;
+  }
+}
 
-  final int offsetBy11Value = (intSBoxResult << 11) | (intSBoxResult >> 21);
-  print('Circle offset');
-  print(offsetBy11Value.to2);
+Uint8List process(Uint8List block, {bool isEncrypte = true}) {
+  var leftPart = block.sublist(0, 4).fromListToInt();
+  var rightPart = block.sublist(4).fromListToInt();
 
-  final int bitSum = offsetBy11Value | leftPartInit;
-  print(leftPartInit.to2);
-  print('bitSum');
-  print(bitSum.to2);
+  for (int roundNumber = 0; roundNumber < 32; roundNumber++) {
+    final keyIndex = (getKeyIndex(roundNumber, isEncrypte));
+    print('keyIndex: $keyIndex\n');
+    final subKey = key[keyIndex];
+    print(block);
+    print(subKey);
+    final fValue = f(leftPart, subKey);
+    final roundResult = rightPart ^ fValue;
+    if (roundNumber < 31) {
+      rightPart = leftPart;
+      leftPart = roundResult;
+    } else {
+      rightPart = roundResult;
+    }
+  }
 
-  final result = [bitSum, leftPartInit];
-  print('\nresult');
-  print('$result\n');
+  return [...leftPart.toBits(32), ...rightPart.toBits(32)].bitsToBytes();
+}
+
+int f(int leftPart, int subkey) {
+  int block = (leftPart + subkey) % pow(2, 32).toInt();
+  block = sBoxConvert(block);
+
+  final blockBits = block.toBits(32);
+  block = [...blockBits.sublist(11), ...blockBits.sublist(0, 11)].toInt();
+  return block;
+}
+
+int sBoxConvert(int block) {
+  int result = 0;
+  List<int> bits = block.toBits(32);
+  Uint8List sBlockResults = Uint8List(8);
+
+  for (var i = 0; i < 8; i++) {
+    final int sIndex = (i % 2 == 0 ? bits.sublist((i + 1) * 4, (i + 2) * 4) : bits.sublist((i - 1) * 4, i * 4)).toInt();
+    final int sBlock = sBoxes[i][sIndex];
+    sBlockResults[i % 2 == 0 ? i + 1 : i - 1] = sBlock;
+  }
+  print(sBlockResults);
+
+  result = sBlockResults.toBits(4).toInt();
   return result;
 }
